fc30b9172ad72639448087fc98d36a5a4481dcce
Dalma Boros <dalma.boros@gmail.com>
2026-05-04 21:46:09 -0500

    Apply set_x_presenter convention to onboarding and dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  before_action :set_dashboard_presenters, only: [:index]
+
   def index
     @hobbies = current_user.hobbies.order(:name)
+  end
+
+  private
+
+  def set_dashboard_presenters
+    @user_presenter = UserPresenter.new(current_user)
+    current_event = current_user.events.active.first
+    @current_event_presenter = EventPresenter.new(current_event) if current_event
   end
 end
